73210337fc67256158bdd012e5b1357ef61cc7e5
abisny <abisny@umich.edu>
2018-02-22 15:46:41 -0500

    office hrs fixed weird block stuff

--- SI364midterm.py
+++ SI364midterm.py
@@ -10,6 +10,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField # Note that you may need to import more here! Check out examples that do what you want to figure out what.
 from wtforms.validators import Required # Here, too
 from flask_sqlalchemy import SQLAlchemy
+from imdb import IMDb # pip install imdbpy
 
 ## App setup code
 app = Flask(__name__)
@@ -17,8 +18,8 @@ app.debug = True
 app.use_reloader = True
 
 ## All app.config values
-app.config['SECRET_KEY'] = 'hard to guess string from si364'
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://AbbySnyder@localhost/abisnyMidterm"
+app.config['SECRET_KEY'] = 'secretstring364midterm'
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://AbbySnyder@localhost/abisnyMidterm"
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -41,11 +42,25 @@ class Name(db.Model):
     __tablename__ = "names"
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(64))
-
     def __repr__(self):
         return "{} (ID: {})".format(self.name, self.id)
 
+class Movie(db.Model):
+    __tablename__ = "movies"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(64))
+    release_year = db.Column(db.Integer)
+    genre = db.Column(db.String(64), db.ForeignKey('genres.name'))
+    def __repr__(self):
+        return "{}, {} (ID: {})".format(self.title, self.release_year, self.id)
 
+class Genre(db.Model):
+    __tablename__ = "genres"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    movies = db.relationship('Movie', backref='movie')
+    def __repr__(self):
+        return "{} (ID: {})".format(self.name, self.id)
 
 ###################
 ###### FORMS ######
@@ -55,6 +70,9 @@ class NameForm(FlaskForm):
     name = StringField("Please enter your name.",validators=[Required()])
     submit = SubmitField()
 
+class MovieForm(FlaskForm):
+    title = StringField("Please enter the title of a movie.", validators=[Required()])
+    submit = SubmitField()
 
 
 #######################
@@ -69,13 +87,15 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+##
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
     form = NameForm() # User should be able to enter name after name and each one will be saved, even if it's a duplicate! Sends data with GET
     if form.validate_on_submit():
+        print ('validated')
         name = form.name.data
-        newname = Name(name)
+        newname = Name(name=name)
         db.session.add(newname)
         db.session.commit()
         return redirect(url_for('all_names'))
@@ -86,10 +106,24 @@ def all_names():
     names = Name.query.all()
     return render_template('name_example.html',names=names)
 
+@app.route('/movies', methods=['GET', 'POST'])
+def movies():
+    form = MovieForm()
+    if form.validate_on_submit():
+        ia = IMDb()
+        first_result = ia.search_movie(form.title.data)[0]
+        release_year = first_result['long imdb canonical title'].split('(')[1].split(')')[0]
+        movie = get_or_create_movie(title=form.title.data, release_year=release_year)
+        db.session.add(movie)
+        db.session.commit()
+        movies = Movie.query.all()
+        return redirect(url_for('all_movies'))
+    return render_template('movie_form.html', form=form)
 
-
-
-
+@app.route('/all_movies')
+def all_movies():
+    movies = Movie.query.all()
+    return render_template('all_movies.html', movies=movies)
 
 ## Code to run the application...
 if __name__ == '__main__':
